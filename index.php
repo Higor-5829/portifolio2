d46332d641497084091ed9ad4e1f59d9034b8c5d
jvnrjj <>
2025-02-10 20:22:11 -0300

    aula 10-02

--- index.php
+++ index.php
@@ -3,10 +3,49 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="stylesheet" href="css/styles.cs">
     <title>Portifólio</title>
 </head>
 <body>
+
+    <div class="interface">
+
+        <header class="cabecalho">
+
+            <nav class="menu">
+
+                <div class="logo">
+                    <img src="imagens/logo.png" alt="LOGO" width="150" />
+                </div>
+
+                <ul>
+                    <li>Home</li>
+                    <li>Serviços</li>
+                    <li>Produtos</li>
+                    <li>Portifólio</li>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+                </ul>
+
+            </nav>
+
+            <div class="titles">
+                <h1 class="titulo">Entregando serviços de qualidade</h1>
+                <h1 class="subtitulo">Soluções de TI</h1>
+
+                <p class="paragrafo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut laboriosam est velit voluptate architecto tenetur.</p>
+            </div>
+
+            <div class="solicitacoes">
+                <a href="#">Twitter</a>
+                <a href="#">Facebook</a>
+                <a href="#">Linkedin</a>
+                <a href="#">Instagram</a>
+            </div>
+
+        </header>
+
+    </div>
     
 </body>
 </html>
